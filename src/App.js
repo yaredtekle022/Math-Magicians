419cde70f80d8ae0afd13e340fc1eda6434d65eb
yaredtt <yaredtekle22@gmail.com>
2023-05-04 09:17:12 +0300

    use functional components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="folder">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="folder">
+      <Calculator />
+    </div>
+  );
 }
-
 export default App;
