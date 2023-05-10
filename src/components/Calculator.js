2bdf2650af2f5a838ba0f203485d12407360cf32
yaredtt <yaredtekle22@gmail.com>
2023-05-10 14:48:43 +0300

    add react events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,123 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Calculate from '../logic/calculate';
 
-function Calculator() {
-  return (
-    <div className="Calculator">
-      <div className="row">
-        <div className="res">0</div>
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleChange = (e) => {
+    const res = Calculate(this.state, e.target.innerHTML);
+    this.setState(res);
+  };
+
+  render() {
+    return (
+      <div className="Calculator">
+        <form className="row">
+          <input
+            disabled
+            className="res"
+            value={this.state.next || this.state.total || 0}
+          />
+        </form>
+        <div className="row">
+          <button
+            type="button"
+            id="AC"
+            onClick={this.handleChange}
+            className="pads"
+          >
+            AC
+          </button>
+          <button
+            type="button"
+            onClick={this.handleChange}
+            id="+/-"
+            className="pads"
+          >
+            +/-
+          </button>
+          <button
+            type="button"
+            onClick={this.handleChange}
+            id="%"
+            className="pads"
+          >
+            %
+          </button>
+          <button type="button" onClick={this.handleChange} className="pads">
+            &#247;
+          </button>
+        </div>
+        <div className="row">
+          <button className="pads" type="button" onClick={this.handleChange}>
+            7
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            8
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            9
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            x
+          </button>
+        </div>
+        <div className="row">
+          <button className="pads" type="button" onClick={this.handleChange}>
+            4
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            5
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            6
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            -
+          </button>
+        </div>
+        <div className="row">
+          <button className="pads" type="button" onClick={this.handleChange}>
+            1
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            2
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            3
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            +
+          </button>
+        </div>
+        <div className="row">
+          <button
+            className="pads zero"
+            type="button"
+            onClick={this.handleChange}
+          >
+            0
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            .
+          </button>
+          <button className="pads" type="button" onClick={this.handleChange}>
+            =
+          </button>
+        </div>
       </div>
-      <div className="row">
-        <div className="pads">AC</div>
-        <div className="pads">+/-</div>
-        <div className="pads">%</div>
-        <div className="pads">&#247;</div>
-      </div>
-      <div className="row">
-        <div className="pads">7</div>
-        <div className="pads">8</div>
-        <div className="pads">9</div>
-        <div className="pads">x</div>
-      </div>
-      <div className="row">
-        <div className="pads">4</div>
-        <div className="pads">5</div>
-        <div className="pads">6</div>
-        <div className="pads">-</div>
-      </div>
-      <div className="row">
-        <div className="pads">1</div>
-        <div className="pads">2</div>
-        <div className="pads">3</div>
-        <div className="pads">+</div>
-      </div>
-      <div className="row">
-        <div className="pads zero">0</div>
-        <div className="pads">.</div>
-        <div className="pads">=</div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
 export default Calculator;
